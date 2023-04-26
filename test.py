947ece21ebc53f458cd072962c2ab9b53c136ca2
PizzaPrix <seanwaterloo2997@gmail.com>
2023-04-26 13:46:41 -0400

    Several Changes
    - Incorporate all columns of array [1:24] rather than having [1:21]
    - FIX: edited_grasping_dataset normalize properly declared
    - Remove reduntant .to(device)

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
-import pandas as pd
-import seaborn as sns
 import numpy as np
 
-# process concatenated data to pass into seaborn pairplot
-column_names = list
-graph_combined_X = np.delete(combined_X, np.s_[-2:], axis=0) # remove last two rows
-graph_combined_X = pd.DataFrame(graph_combined_X)
-graph_combined_X['labels'] = combined_y
-sns.pairplot(graph_combined_X, vars=['labels'], hue="labels")
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(arr)
+arr = np.delete(arr, 1, 1)
+print(arr)
