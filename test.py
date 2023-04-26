d48b2e14608b3aa3394516b646f4c69726ceef96
PizzaPrix <seanwaterloo2997@gmail.com>
2023-04-26 12:41:53 -0400

    Set Blocksize = 1

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
+import pandas as pd
+import seaborn as sns
 import numpy as np
-import math
 
-def normalize_vector(nd_arr: np.ndarray) -> np.ndarray:
-    vector_length = sum([i ** 2 for i in nd_arr]) ** 0.5
-    normalized_arr = [i / vector_length for i in nd_arr]
-    return normalized_arr
-
-arr = np.array([[3, 4], [5, 3]])
-noramlized_arr = [normalize_vector(row) for row in arr]
-print(noramlized_arr)
+# process concatenated data to pass into seaborn pairplot
+column_names = list
+graph_combined_X = np.delete(combined_X, np.s_[-2:], axis=0) # remove last two rows
+graph_combined_X = pd.DataFrame(graph_combined_X)
+graph_combined_X['labels'] = combined_y
+sns.pairplot(graph_combined_X, vars=['labels'], hue="labels")
